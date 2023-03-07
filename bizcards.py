9bee851f27f394f7d05ce75244fca5116c01b17d
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-08 00:49:19 +0530

    Update bizcards.py

--- bizcards.py
+++ bizcards.py
@@ -3,23 +3,24 @@ import easyocr
 import cv2
 import numpy as np
 
-st.set_page_config(page_title='Text Blocks Detection with EasyOCR')
-st.title('Text Blocks Detection with EasyOCR')
-st.write('Upload an image and the app will detect the text blocks in it.')
+st.set_page_config(page_title='Text Detection with EasyOCR')
+st.title('Text Detection with EasyOCR')
+st.write('Upload an image and the app will detect the text in it.')
 uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
 if uploaded_file is not None:
     image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
     reader = easyocr.Reader(['en'])
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 5))
+    morph = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+    thresh = cv2.threshold(morph, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    text_blocks = []
+    boxes = []
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         roi = image[y:y + h, x:x + w]
         results = reader.readtext(roi)
-        if results:
-            text = [result[1] for result in results]
-            text_blocks.append((text, [x, y, x + w, y + h]))
-    for i, block in enumerate(text_blocks):
-        st.write(f'Text Block {i+1}:\nText: {block[0]}\nBounding Box: {block[1]}')
+        if results and results[0]:
+            boxes.append(([x, y, x + w, y + h], results[0][1]))
+    for i, box in enumerate(boxes):
+        st.write(f'Text {i+1}: {box[1]}')
