1fc1147939ab6e0adde5b43104942100c85ae0a8
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-08 14:40:24 +0530

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,46 +1,42 @@
-import streamlit as st
 import cv2
-import numpy as np
 import pytesseract
-from google.cloud import vision
-from google.cloud.vision_v1.types import Image
+import streamlit as st
+from PIL import Image
 
-# Authenticate with Google Cloud Vision API
-client = vision.ImageAnnotatorClient()
+st.set_page_config(page_title="Text Recognition", page_icon=":pencil:", layout="wide")
 
-# Set up Streamlit app
-st.set_page_config(page_title="Image Text Recognition", page_icon=":camera:")
+# Define Tesseract configuration options
+config = '--psm 6'
+
+# Define Streamlit app
+st.title("Text Recognition")
 
 # Upload image
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Load image
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-    # Convert image to grayscale
+    # Load image and convert to grayscale
+    image = Image.open(uploaded_file)
+    img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Apply thresholding to remove noise
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    # Binarize the image using adaptive thresholding
+    binarized = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, blockSize=15, C=5)
 
-    # Apply dilation to merge text into single blocks
-    kernel = np.ones((5,5), np.uint8)
-    dilation = cv2.dilate(thresh, kernel, iterations=5)
+    # Find contours in the binarized image
+    contours, hierarchy = cv2.findContours(binarized, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Detect text blocks using Google Cloud Vision API
-    image = Image(content=cv2.imencode('.jpg', dilation)[1].tostring())
-    response = client.document_text_detection(image=image)
-    blocks = response.full_text_annotation.pages[0].blocks
+    # Iterate through each contour and find text blocks
+    text_blocks = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if w > 100 and h > 10:
+            # Crop the text block from the original image
+            block = img[y:y+h, x:x+w]
+            text_blocks.append(block)
 
-    # Draw text blocks on image
-    for block in blocks:
-        for paragraph in block.paragraphs:
-            for word in paragraph.words:
-                for symbol in word.symbols:
-                    vertices = symbol.bounding_box.vertices
-                    cv2.rectangle(img, (vertices[0].x, vertices[0].y), (vertices[2].x, vertices[2].y), (0, 255, 0), 2)
-
-    # Display image
-    st.image(img, caption="Processed image with text blocks", use_column_width=True)
+    # Apply OCR to each text block and print the recognized text and text block image
+    for i, block in enumerate(text_blocks):
+        text = pytesseract.image_to_string(block, config=config)
+        st.write(f"Text Block {i+1}: {text}")
+        st.image(block, caption=f"Text Block {i+1} Image", use_column_width=True)
