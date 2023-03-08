430ca5c95497392a44fa32d472fba29f089f0f8f
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-08 22:56:44 +0530

    Update My_App.py

--- My_App.py
+++ My_App.py
@@ -125,15 +125,6 @@ if image is not None:
                         if len(numbers) == 0 or all(len(num) < 3 for num in numbers) and not any(num in string for num in ['0','1','2','3','4','5','6','7','8','9']*3):
                             fin.append(string)
         st.write('##### :red[CARD HOLDER & COMPANY DETAILS: ] ')
-        st.table([fin])
-
-#         if len(fin) % 2 == 0:
-#             # If the list has an even number of elements
-#             for i in range(0, len(fin), 2):
-#                 st.write('##### '+fin[i:i+2])
-#         else:
-#             # If the list has an odd number of elements
-#             for i in range(0, len(fin)-1, 2):
-#                 st.write('##### '+fin[i:i+2])
-#             st.write('##### '+[fin[-1]])
+        table_data = [[fins] for fins in fin]
+        st.table(table_data)
 
