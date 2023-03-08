fe9a99774bf6868fcd65ec5ff730a32fc1e4aae0
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-08 14:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ st.title("Image Text Block Margins")
 # Upload image file
 uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
+# Define default thresholding parameters
+default_thresh_val = 127
+default_thresh_type = "Binary"
+default_thresh_mode = "OTSU"
+
+# Define Streamlit sliders for adjusting thresholding parameters
+thresh_val = st.slider("Threshold value", min_value=0, max_value=255, value=default_thresh_val)
+thresh_type = st.selectbox("Threshold type", options=["Binary", "Binary Inv"], index=0)
+thresh_mode = st.selectbox("Threshold mode", options=["OTSU", "Adaptive"], index=0)
+block_size = st.slider("Block size (for adaptive thresholding)", min_value=3, max_value=99, value=11, step=2)
+
 # Display image with text block margins
 if uploaded_file is not None:
     # Read image file as numpy array
@@ -18,8 +29,23 @@ if uploaded_file is not None:
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # Apply pre-processing to reduce noise and improve text recognition
+    gray = cv2.medianBlur(gray, 3)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
     # Apply thresholding to remove noise
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    if thresh_mode == "OTSU":
+        thresh_mode_flag = cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+    else:
+        thresh_mode_flag = cv2.THRESH_BINARY_INV
+    if thresh_type == "Binary":
+        thresh_type_flag = cv2.THRESH_BINARY
+    else:
+        thresh_type_flag = cv2.THRESH_BINARY_INV
+    if thresh_mode == "Adaptive":
+        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresh_type_flag, block_size, 2)
+    else:
+        _, thresh = cv2.threshold(gray, thresh_val, 255, thresh_type_flag + thresh_mode_flag)
 
     # Find contours in the image
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
