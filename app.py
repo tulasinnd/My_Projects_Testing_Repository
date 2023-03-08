61f12748959a23d80fe07869feb28f39295b126a
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-08 14:06:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
 import cv2
-import pytesseract
 import numpy as np
+import streamlit as st
 
-# Load image
-img = cv2.imread('image.jpg')
+# Set up Streamlit app
+st.set_page_config(page_title="Image Text Block Margins", layout="wide")
+st.title("Image Text Block Margins")
 
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Upload image file
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
-# Apply thresholding to remove noise
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+# Display image with text block margins
+if uploaded_file is not None:
+    # Read image file as numpy array
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-# Find contours in the image
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Iterate through contours and draw bounding boxes around text blocks
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
-    if w > 50 and h > 50:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Apply thresholding to remove noise
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-# Display the new image with bounding boxes around text blocks
-cv2.imshow('Image with Text Blocks', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Find contours in the image
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Iterate through contours and draw bounding boxes around text blocks
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w > 50 and h > 50:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the new image with bounding boxes around text blocks
+    st.image(img, channels="BGR")
+else:
+    st.warning("Please upload an image file.")
